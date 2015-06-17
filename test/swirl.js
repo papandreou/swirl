7687d6038c0b304d5fdb8e2a65c55fec1d73bb8f
Andreas Lind <andreas@one.com>
2015-06-17 09:38:41 +0200

    Update unexpected etc.

--- test/swirl.js
+++ test/swirl.js
@@ -3,95 +3,37 @@ var childProcess = require('child_process'),
     pathModule = require('path'),
     unexpected = require('unexpected');
 
-function makeMakeCallback(done) {
-    var numOutstandingCallbacks = 0,
-        doneCalled = false;
-    return function makeCallback(fn) {
-        numOutstandingCallbacks += 1;
-        return function (err) {
-            var errorToRethrow;
-            if (fn) {
-                try {
-                    fn.apply(this, arguments);
-                } catch (e) {
-                    errorToRethrow = e;
-                }
-            }
-            numOutstandingCallbacks -= 1;
-            if (err && !fn) {
-                if (doneCalled) {
-                    throw err;
-                } else {
-                    doneCalled = true;
-                    done(err);
-                }
-            } else if (numOutstandingCallbacks === 0 && !doneCalled) {
-                doneCalled = true;
-                done();
-            }
-            if (errorToRethrow) {
-                throw errorToRethrow;
-            }
-        };
-    };
-}
-
 describe('swirl', function () {
     var expect = unexpected.clone()
-        .addAssertion('to exit with', function (expect, subject, value, done) {
-            subject.on('exit', function (exitCode) {
-                expect(exitCode, 'to equal', value);
-                done();
+        .installPlugin(require('unexpected-stream'))
+        .addAssertion('to exit with', function (expect, subject, value) {
+            this.errorMode = 'nested';
+            return expect.promise(function (run) {
+                subject.on('exit', run(function (exitCode) {
+                    expect(exitCode, 'to equal', value);
+                }));
             });
         })
-        .addAssertion('to exit successfully', function (expect, subject, done) {
-            expect(subject, 'to exit with', 0, done);
-        })
-        .addAssertion('to be a readable stream that outputs', function (expect, subject, value, done) {
-            expect(done, 'to be a function');
-            this.args.pop();
-            var chunks = [];
-            subject.on('data', function (chunk) {
-                if (typeof chunk === 'string') {
-                    chunk = new Buffer(chunk);
-                }
-                chunks.push(chunk);
-            }).on('end', function () {
-                var output = Buffer.concat(chunks),
-                    valueIsRegExp = Object.prototype.toString.call(value) === '[object RegExp]';
-                if (typeof value === 'string' || valueIsRegExp) {
-                    output = output.toString('utf-8');
-                }
-                var err;
-                try {
-                    expect(output, 'to satisfy', value);
-                } catch (e) {
-                    err = e;
-                }
-                if (typeof done === 'function') {
-                    setImmediate(function () {
-                        done(err);
-                    });
-                } else if (err) {
-                    throw err;
-                }
-            }).on('error', done);
+        .addAssertion('to exit successfully', function (expect, subject) {
+            this.errorMode = 'nested';
+            return expect(subject, 'to exit with', 0);
         });
 
-    it('should produce some output', function (done) {
-        var makeCallback = makeMakeCallback(done),
-            swirl = childProcess.spawn(pathModule.resolve(__dirname, '..', 'bin', 'swirl'), [
-                '-H', 'Foo: Bar',
-                'google.com',
-                'statusCode:412'
-            ]);
-        swirl.stdout.on('data', function () {});
-        expect(swirl, 'to exit with', 1, makeCallback());
-        expect(
-            swirl.stderr,
-            'to be a readable stream that outputs',
-            /HTTP\/1\.1 .*\/\/ should be 412 Precondition Failed/,
-            makeCallback());
-        swirl.stdin.end();
+    it('should produce some output', function () {
+        var swirl = childProcess.spawn(pathModule.resolve(__dirname, '..', 'bin', 'swirl'), [
+            '-H', 'Foo: Bar',
+            'google.com',
+            'statusCode:412'
+        ]);
+        return expect.promise.all([
+            expect(swirl, 'to exit with', 1),
+            expect(
+                swirl.stderr,
+                'to yield output satisfying',
+                'when decoded as', 'utf-8',
+                'to satisfy',
+                /HTTP\/1\.1 .*\/\/ should be 412 Precondition Failed/
+            )
+        ]);
     });
 });
